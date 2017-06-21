0f456c97d1f85061de43e76ba284604cc728d03e
Mike Harsch <mike@harschsystems.com>
2017-06-21 15:02:07 -0600

    packagize

---
+++ plates.js
@@ -0,0 +1,9 @@
+var RELAYplate = require('./RELAYplate');
+var DAQCplate = require('./DAQCplate');
+var MOTORplate = require('./MOTORplate');
+
+module.exports = {
+	RELAYplate: RELAYplate,
+	DAQCplate: DAQCplate,
+	MOTORplate: MOTORplate
+}
