a754462de9fe81fbb8893ed672c82649a8ca0e66
David Baltusavich <david@synatree.com>
2018-08-03 14:35:17 -0400

    NPM init

--- RELAYplate.js
+++ RELAYplate.js
@@ -4,6 +4,24 @@ class RELAYplate extends BASEplate {
     constructor (addr) {
         super(addr, "RELAY");
     }
+
+    async relayOn(num){
+        return await new Promise((resolve,reject)=>{
+            this.send({cmd: "relayON", args: { relay: num}}, resolve);
+        });  
+    }
+
+    async relayOff(num){
+        return await new Promise((resolve,reject)=>{
+            this.send({cmd: "relayOFF", args: { relay: num}}, resolve);
+        });
+    }
+
+    async relayToggle(num){
+        return await new Promise((resolve,reject)=>{
+            this.send({cmd: "relayTOGGLE", args: { relay: num}}, resolve);
+        });
+    }
 }
 
 module.exports = RELAYplate;
