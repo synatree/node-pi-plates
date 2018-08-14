4351653f0c6e181fd93c731b0fe371eac2b97b86
David Baltusavich <david@synatree.com>
2018-08-14 15:58:10 -0400

    updates for governor

--- RELAYplate.js
+++ RELAYplate.js
@@ -1,25 +1,58 @@
 const BASEplate = require('./BASEplate');
-
+var lock = new AwaitLock();
 class RELAYplate extends BASEplate {
-    constructor (addr) {
+    constructor (addr, governor=0) {
         super(addr, "RELAY");
+        if(governor)
+        {
+            // it's important to know how many are currently on so that we can enforce the governing rule.
+            this.totalOn = 0;
+            [0,1,2,3,4,5,6,7].forEach((n)=>{
+                var v = await this.relayState(n);
+                if(v)
+                {
+                    this.totalOn++;
+                }
+            });
+            this.governor = governor;
+        }
+        
     }
 
     async relayOn(num){
+        
         return await new Promise((resolve,reject)=>{
+         
+            if(this.totalOn+1 > this.governor)
+            {
+                console.log("Cannot Turn On Relay, Too Many Open");
+                reject();
+            }
             this.send({cmd: "relayON", args: { relay: num}}, resolve);
+        }).then(()=>{
+            this.totalOn++;
         });  
     }
 
     async relayOff(num){
         return await new Promise((resolve,reject)=>{
             this.send({cmd: "relayOFF", args: { relay: num}}, resolve);
+        }).then(()=>{
+            this.totalOn--;
         });
     }
 
     async relayToggle(num){
         return await new Promise((resolve,reject)=>{
-            this.send({cmd: "relayTOGGLE", args: { relay: num}}, resolve);
+            var cur = await this.relayState(num)
+            if(! cur)
+            {
+                return this.relayOn(num)
+            }
+            else
+            {
+                return this.relayOff(num);
+            }
         });
     }
 
@@ -28,6 +61,8 @@ class RELAYplate extends BASEplate {
             this.send({cmd: "relaySTATE", args: { relay: num}}, resolve);
         });
     }
+
+
 }
 
 module.exports = RELAYplate;
