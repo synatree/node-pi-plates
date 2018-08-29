c13b58c5cf9e43681ac9af5acc2d9fe29850ac84
David Baltusavich <david@synatree.com>
2018-08-29 18:09:45 -0400

    PSN-57

--- DAQCplate.js
+++ DAQCplate.js
@@ -4,6 +4,41 @@ class DAQCplate extends BASEplate {
     constructor (addr) {
         super(addr, "DAQC");
     }
+
+    async clearDOUT(bit)
+    {
+        return await new Promise((resolve,reject)=>{
+            this.send({cmd: "clrDOUTbit", args: { bit }}, resolve);
+        });
+    }
+
+    
+    async setDOUT(bit)
+    {
+        return await new Promise((resolve,reject)=>{
+            this.send({cmd: "setDOUTbit", args: { bit }}, resolve);
+        });
+    }
+    async setDIN(bit)
+    {
+        return await new Promise((resolve,reject)=>{
+            this.send({cmd: "setDINbit", args: { bit }}, resolve);
+        });
+    }
+
+    async getDIN(bit)
+    {
+        return await new Promise((resolve,reject)=>{
+            this.send({cmd: "getDINbit", args: { bit }}, resolve);
+        });
+    }
+
+    async getDOUT(bit)
+    {
+        return await new Promise((resolve,reject)=>{
+            this.send({cmd: "getDOUTbit", args: { bit }}, resolve);
+        });
+    }
 }
 
 module.exports = DAQCplate;
